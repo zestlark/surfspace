2235bf870992e602c6ae72151c6160627d4e9c54
Deepak Kanojiya <kanojiyadeepak747@gmail.com>
2024-06-26 00:30:04 +0530

    updated blog card shows

--- src/component/ZBlog.js
+++ src/component/ZBlog.js
@@ -35,7 +35,7 @@ const ZBlog = () => {
                 {zblogData.length > 0 ? zblogData.reverse().splice(0, 5).map((blog, index) => {
                     return (
                         <div key={index} className='w-full pb-3 sm:p-1'>
-                            <div className="card dark:bg-slate-800 max-w-[full] bg-base-100 w-full h-full shadow-xl aspect-video">
+                            <div className="card dark:bg-slate-800 max-w-[full] bg-base-100 w-full h-full shadow-sm aspect-video">
                                 <figure>
                                     <img className='aspect-video object-cover'
                                         src={blog.imageUrl}
